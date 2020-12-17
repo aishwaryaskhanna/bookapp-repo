af962cd05ff97ecf19fc487db8eca1c934637b37
aishwaryaskhanna <54758572+aishwaryaskhanna@users.noreply.github.com>
2020-12-17 08:05:22 -0800

    9.3

--- lib/components/download_popup.dart
+++ lib/components/download_popup.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:book_app/components/custom_alert.dart';
 import 'package:book_app/utility/formatter.dart';
 
 class DownloadPopup extends StatefulWidget {
@@ -49,29 +48,36 @@ class _DownloadPopupState extends State<DownloadPopup> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () => Future.value(false),
-      child: CustomAlert(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                'Downloading the book..',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
+      child: Container(
+        color: Colors.black,
+        child: Card(
+          color: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  'Downloading the book..',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              SizedBox(height: 20.0),
-              CircularProgressIndicator(
-                valueColor:
-                    new AlwaysStoppedAnimation<Color>(Colors.cyanAccent),
-              ),
-            ],
+                SizedBox(height: 20.0),
+                CircularProgressIndicator(
+                  valueColor:
+                      new AlwaysStoppedAnimation<Color>(Colors.cyanAccent),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/components/exit_popup.dart
+++ lib/components/exit_popup.dart
@@ -1,74 +1,29 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:book_app/components/custom_alert.dart';
 
 class ExitPopup {
   showExitDialog(BuildContext context) {
     showDialog(
       context: context,
-      builder: (context) => CustomAlert(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              SizedBox(height: 25.0),
-              Text(
-                'Press OK to exit the app.',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 14.0,
-                ),
-              ),
-              SizedBox(height: 40.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 40.0,
-                    width: 130.0,
-                    child: OutlineButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      borderSide:
-                          BorderSide(color: Theme.of(context).accentColor),
-                      child: Text(
-                        'Cancel',
-                        style: TextStyle(
-                          color: Theme.of(context).accentColor,
-                        ),
-                      ),
-                      onPressed: () => Navigator.pop(context),
-                      color: Colors.white,
-                    ),
-                  ),
-                  Container(
-                    height: 40.0,
-                    width: 130.0,
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      child: Text(
-                        'OK',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      onPressed: () => exit(0),
-                      color: Theme.of(context).accentColor,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20.0),
-            ],
+      builder: (context) => AlertDialog(
+        title: Text(
+          'Are you sure?',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20.0,
           ),
         ),
+        content: Text('Press OK to exit the app.'),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text('Cancel'),
+          ),
+          FlatButton(
+            onPressed: () => exit(0),
+            child: Text('OK'),
+          ),
+        ],
       ),
     );
   }
